0a7f515fc2f52fa70575d54e3784c29455b99aa7
finleyuk8 <sweeneyfinley@gmail.com>
2024-03-11 20:11:45 +0000

    Added simple player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    //Movement control
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Vector2 moveDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputManagement();
+
     }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManagement() //Get the input from the player
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move() //Move the player
+    {
+        rb.velocity = moveDirection * moveSpeed;
+    }
+
 }
+
